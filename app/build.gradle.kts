dc53b3a297d1f49b47091ae9a959d711d2fe112c
Karol Zając <karolzajac.0407@gmail.com>
2024-10-24 20:08:12 +0200

    add material theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- app/src/main/java/com/pwr/wanderway/MainActivity.kt
+++ app/src/main/java/com/pwr/wanderway/MainActivity.kt
@@ -4,23 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.pwr.wanderway.ui.theme.WanderWayTheme
+import com.pwr.wanderway.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WanderWayTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.primary // Using background from theme
                 ) {
                     Greeting("Android")
                 }
@@ -31,16 +33,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Button(
+
+        onClick = { /* TODO: Add your click action */ },
         modifier = modifier
-    )
+    ) {
+        Text(
+            style = MaterialTheme.typography.displayLarge,
+            text = "Hello $name!"
+        ) // Text inside the Button
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    WanderWayTheme {
+    AppTheme {
         Greeting("Android")
     }
 }

aa1e5b9d5c1031272b1161325c0a74d1ad7b4339
Karol Zając <122168670+poziomk3@users.noreply.github.com>
2024-11-07 20:33:09 +0100

    Feature build your route screen (#9)
    Co-authored-by: therockey <kacperus.tomczyk@gmail.com>

--- app/src/main/java/com/pwr/wanderway/presentation/routeCore/home/HomeScreen.kt
+++ app/src/main/java/com/pwr/wanderway/presentation/routeCore/home/HomeScreen.kt
@@ -1,31 +1,88 @@
 package com.pwr.wanderway.presentation.routeCore.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-@Composable
-fun HomeScreen() {
-    Surface(
-        color = MaterialTheme.colorScheme.errorContainer,
-        modifier = Modifier.fillMaxSize()
+import androidx.compose.ui.unit.dp
+import com.pwr.wanderway.presentation.commons.ButtonColor
+import com.pwr.wanderway.presentation.commons.WideButton
+import com.pwr.wanderway.ui.theme.AppTheme
 
-    )
-    {
-        Column ( verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Home Screen")
+@Composable
+fun HomeScreen(
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                Text(
+                    text = "Ready for an adventure?",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Let’s start with a simple question....",
+                        style = MaterialTheme.typography.titleSmall.copy(fontStyle = FontStyle.Italic),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+
+                    Text(
+                        text = "What do you wanna see???",
+                        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.ExtraBold),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 16.dp)
+                    .weight(1f)
+                    .background(MaterialTheme.colorScheme.errorContainer)
+            )
+
+            WideButton(ButtonColor.PRIMARY, "Test Button", onClick = onClick)
         }
     }
 }
+
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    AppTheme {
+        HomeScreen(onClick = {})
+    }
 }

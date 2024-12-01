d24cea1bd05f995938fec16727148bb34ef4f73b
Karol Zając <karolzajac.0407@gmail.com>
2024-12-01 22:01:14 +0100

    remove unnnecessary imports

--- app/src/main/java/com/pwr/wanderway/navigation/authorized/AuthorizedWrapper.kt
+++ app/src/main/java/com/pwr/wanderway/navigation/authorized/AuthorizedWrapper.kt
@@ -1,10 +1,8 @@
 package com.pwr.wanderway.navigation.authorized
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue

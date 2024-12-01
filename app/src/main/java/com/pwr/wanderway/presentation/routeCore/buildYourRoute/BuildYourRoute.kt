133f07915c4b5758f2510356df16e89fb3c6069f
therockey <kacperus.tomczyk@gmail.com>
2024-12-02 00:28:08 +0100

    Extract string resource

--- app/src/main/java/com/pwr/wanderway/presentation/routeCore/buildYourRoute/BuildYourRoute.kt
+++ app/src/main/java/com/pwr/wanderway/presentation/routeCore/buildYourRoute/BuildYourRoute.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pwr.wanderway.R
 import com.pwr.wanderway.data.model.RowSelectorConfig
 import com.pwr.wanderway.presentation.commons.ButtonColor
 import com.pwr.wanderway.presentation.commons.WideButton
@@ -29,46 +31,47 @@ fun BuildYourRouteScreen(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize().padding(horizontal = 16.dp),
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ) {
         RowSelector(
             config = RowSelectorConfig(
-                text = "Add starting point",
+                text = stringResource(R.string.add_starting_point),
                 onClick = locationAdditionNav
             )
         )
         RowSelector(
             config = RowSelectorConfig(
-                text = "Add destination",
+                text = stringResource(R.string.add_destination),
                 onClick = { }
             )
         )
         WideButton(
-            text = "Change your personal preferences",
+            text = stringResource(R.string.change_your_personal_preferences),
             onClick = { preferencesNav() },
             colorType = ButtonColor.SECONDARY
         )
         WideButton(
-            text = "Create route",
+            text = stringResource(R.string.create_route),
             onClick = routeChoiceNav,
             colorType = ButtonColor.PRIMARY
         )
         Text(
-            text = "Trip overview",
+            text = stringResource(R.string.trip_overview),
             style = MaterialTheme.typography.titleLarge.copy(fontWeight = androidx.compose.ui.text.font.FontWeight.ExtraBold)
         )
         DestInfo(
-            label = "Starting in:",
+            label = stringResource(R.string.starting_in),
             contents = { Text(text = "Location 1") }
         )
         DestInfo(
-            label = "Finishing in:",
+            label = stringResource(R.string.finishing_in),
             contents = { Text(text = "Destination 1, Destination 2") }
         )
         DestInfo(
-            label = "Attractions not seen yet:",
+            label = stringResource(R.string.attractions_not_seen_yet),
             contents = {
                 LazyColumn {
                     items(buildYourRouteViewModel.unseenDestinations) { config ->

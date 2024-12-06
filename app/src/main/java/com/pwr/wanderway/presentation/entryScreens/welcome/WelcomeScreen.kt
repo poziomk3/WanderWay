6982bb0fe9bc08ce7e46323336ecf70a3a006ed4
Karol Zając <karolzajac.0407@gmail.com>
2024-12-06 21:26:30 +0100

    adjust welcome screen

--- app/src/main/java/com/pwr/wanderway/presentation/entryScreens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/pwr/wanderway/presentation/entryScreens/welcome/WelcomeScreen.kt
@@ -3,12 +3,10 @@ package com.pwr.wanderway.presentation.entryScreens.welcome
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pwr.wanderway.R
 import com.pwr.wanderway.coreViewModels.AuthViewModel
 import com.pwr.wanderway.presentation.entryScreens.commons.EntryScreenLayout
@@ -16,16 +14,13 @@ import com.pwr.wanderway.ui.theme.AppTheme
 
 @Composable
 fun WelcomeScreen(
-    authViewModel: AuthViewModel = hiltViewModel(), // Inject AuthViewModel
-    welcomeViewModel: WelcomeViewModel = viewModel { WelcomeViewModel(authViewModel) }, // Create WelcomeViewModel
+    authViewModel: AuthViewModel = hiltViewModel(),
     onLoginClick: () -> Unit,
     onRegisterClick: () -> Unit,
     onAlreadyLoggedIn: () -> Unit
 ) {
-    // Observe isLoggedIn state using collectAsState
-    val isLoggedIn = welcomeViewModel.isLoggedIn.collectAsState(initial = false)
 
-    if (isLoggedIn.value) {
+    if (authViewModel.isLoggedIn.value) {
         onAlreadyLoggedIn()
     } else {
         EntryScreenLayout(

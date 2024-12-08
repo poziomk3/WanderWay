b242f21de70b2415f462a1f436a97358405b10d6
Karol Zając <karolzajac.0407@gmail.com>
2024-12-08 19:10:35 +0100

    improve toast

--- app/src/main/java/com/pwr/wanderway/utils/notifications/ShowNotification.kt
+++ app/src/main/java/com/pwr/wanderway/utils/notifications/ShowNotification.kt
@@ -1,46 +1,31 @@
 package com.pwr.wanderway.utils.notifications
 
 import android.Manifest
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import com.pwr.wanderway.MainActivity
 import com.pwr.wanderway.R
 
 fun showNotification(context: Context, title: String, message: String) {
     val channelId = "default_channel_id"
     val notificationId = 1
 
-    val intent = Intent(context, MainActivity::class.java)
-    val pendingIntent = PendingIntent.getActivity(
-        context,
-        0,
-        intent,
-        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-    )
+    // Check for POST_NOTIFICATIONS permission (Android 13+)
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return // Permission not granted; exit function
+        }
+    }
 
     val notification = NotificationCompat.Builder(context, channelId)
         .setSmallIcon(R.drawable.icon) // Replace with your app icon
         .setContentTitle(title)
         .setContentText(message)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        .setContentIntent(pendingIntent) // Attach the PendingIntent
-        .setAutoCancel(true) // Auto-dismiss when clicked
+        .setAutoCancel(true)
         .build()
 
-    if (ActivityCompat.checkSelfPermission(
-            context,
-            Manifest.permission.POST_NOTIFICATIONS
-        ) != PackageManager.PERMISSION_GRANTED
-    ) {
-        // Permission not granted, exit the function
-        return
-    }
-
-    // Show the notification
     NotificationManagerCompat.from(context).notify(notificationId, notification)
 }

53bfa30f0d06fdfacb8a30393c42f149e66dec29
Karol Zając <karolzajac.0407@gmail.com>
2024-12-11 12:52:21 +0100

    refactor language change

--- app/src/main/java/com/pwr/wanderway/presentation/accountSettings/settings/LanguageSettingsViewModel.kt
+++ app/src/main/java/com/pwr/wanderway/presentation/accountSettings/settings/LanguageSettingsViewModel.kt
@@ -1,5 +1,6 @@
 package com.pwr.wanderway.presentation.accountSettings.settings
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.pwr.wanderway.data.local.SettingsManager
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,10 +11,21 @@ import javax.inject.Inject
 @HiltViewModel
 class LanguageSettingsViewModel @Inject constructor(
     private val settingsManager: SettingsManager
-): ViewModel() {
+) : ViewModel() {
     fun saveLocale(locale: Locale) {
         runBlocking {
             settingsManager.saveSetting("selected_locale", locale.language)
         }
     }
+
+
+    fun getCurrentLocale(context: Context): Locale {
+        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            context.resources.configuration.locales[0] // For Android N and above
+        } else {
+            @Suppress("DEPRECATION")
+            context.resources.configuration.locale // For older versions
+        }
+    }
+
 }
---
+++ app/src/main/java/com/pwr/wanderway/utils/languages/Languages.kt
@@ -0,0 +1,17 @@
+package com.pwr.wanderway.utils.languages
+
+import java.util.Locale
+
+enum class Languages(val localeCode: String, val locale: Locale, val flagEmoji: String) {
+    ENGLISH("en", Locale("en"), "\uD83C\uDDEC\uD83C\uDDE7"),  // 🇬🇧 for English (UK)
+    POLISH("pl", Locale("pl"), "\uD83C\uDDF5\uD83C\uDDF1"),   // 🇵🇱 for Poland
+    GERMAN("de", Locale("de"), "\uD83C\uDDE9\uD83C\uDDEA"),   // 🇩🇪 for Germany
+    UKRAINIAN("uk", Locale("uk"), "\uD83C\uDDFA\uD83C\uDDE6"), // 🇺🇦 for Ukraine
+    SPANISH("es", Locale("es"), "\uD83C\uDDEA\uD83C\uDDF8");   // 🇪🇸 for Spain
+
+    companion object {
+        fun getFlagByLocaleCode(code: String): String {
+            return entries.find { it.localeCode == code.lowercase() }?.flagEmoji ?: ""
+        }
+    }
+}
